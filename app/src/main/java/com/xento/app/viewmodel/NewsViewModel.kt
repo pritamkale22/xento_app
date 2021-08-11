07964d6481a182e97e6488f5186400ed9951a8f0
kalepritam91@gmail.com <kalepritam91@gmail.com>
2021-08-11 14:39:56 +0530

    current date in API of News

--- app/src/main/java/com/xento/app/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/xento/app/viewmodel/NewsViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.ViewModel
 import com.xento.app.model.news.News
 import com.xento.app.remote.NewsService
 import kotlinx.coroutines.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewsViewModel : ViewModel() {
 
@@ -24,10 +27,13 @@ class NewsViewModel : ViewModel() {
 
     private fun fetchNesw() {
         loading.value = true
+        val sdf = SimpleDateFormat("yyyy-MM-dd")
+        val currentDate = sdf.format(Date())
+
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
 
           //  everything?q=tesla&from=2021-07-10&sortBy=publishedAt&apiKey=1edb7f6f01674626ac4aea23415c60f6
-            val response = newsService.getNews("tesla","2021-07-10","publishedAt","1edb7f6f01674626ac4aea23415c60f6")
+            val response = newsService.getNews("tesla", currentDate, "publishedAt", "1edb7f6f01674626ac4aea23415c60f6")
             withContext(Dispatchers.Main) {
                 if (response.isSuccessful) {
                     news.value = response.body()?.data
